4b1e400a0bf1ebe35cdc8b2bbde4bbd6bb84765f
alaref96 <abdulkader.alaref@gmail.com>
2023-01-23 22:30:38 +0100

    new photos

--- src/service/model.ts
+++ src/service/model.ts
@@ -21,18 +21,38 @@ export const allSlideImageItems: SlideImageItem[] = [
     text: "lorem ipsum Hallo",
   },
   {
-    imageName: "aleppo_castle.jpg",
+    imageName: "Citadel_of_Aleppo 2010.jpg",
     slideName: "forth_slide",
     text: "lorem ipsum Hallo",
   },
   {
-    imageName: "Citadel_of_Aleppo 2010.jpg",
+    imageName: "aleppo_castle.jpg",
     slideName: "fifth_slide",
     text: "lorem ipsum Hallo",
   },
   {
-    imageName: "aleppocas.jpg",
+    imageName: "1 (5).jpg",
     slideName: "sixth_slide",
+    text: "lorem ipsum Hallo",
+  },
+  {
+    imageName: "1 (7).jpg",
+    slideName: "seventh_slide",
+    text: "lorem ipsum Hallo",
+  },
+  {
+    imageName: "1 (4).jpg",
+    slideName: "eighth_slide",
+    text: "lorem ipsum Hallo",
+  },
+  {
+    imageName: "1(20).jpg",
+    slideName: "ninth_slide",
+    text: "lorem ipsum Hallo",
+  },
+  {
+    imageName: "St George's Church and Tawhid Mosque.jpg",
+    slideName: "tenth_slide",
     text: "lorem ipsum Hallo",
   },
 ];
