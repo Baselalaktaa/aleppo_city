8f6d2a3331e69cac3aba7789945922cb5c5a595a
alaref96 <abdulkader.alaref@gmail.com>
2023-01-23 01:23:59 +0100

    photos changed

--- src/service/model.ts
+++ src/service/model.ts
@@ -8,31 +8,31 @@ export const allSlideImageItems: SlideImageItem[] = [
   {
     imageName: "13 September 1920.jpg",
     slideName: "first_slide",
-    text: "lorem ipsum sadasdäasfälaf",
+    text: "lorem ipsum Hallo",
   },
   {
     imageName: "1922.jpg",
     slideName: "second_slide",
-    text: "lorem ipsum sadasdäasfälaf",
+    text: "lorem ipsum Hallo",
   },
   {
     imageName: "1974.jpg",
     slideName: "third_slide",
-    text: "lorem ipsum sadasdäasfälaf",
+    text: "lorem ipsum Hallo",
   },
   {
     imageName: "aleppo_castle.jpg",
     slideName: "forth_slide",
-    text: "lorem ipsum sadasdäasfälaf",
+    text: "lorem ipsum Hallo",
   },
   {
     imageName: "Citadel_of_Aleppo 2010.jpg",
     slideName: "fifth_slide",
-    text: "lorem ipsum sadasdäasfälaf",
+    text: "lorem ipsum Hallo",
   },
   {
     imageName: "aleppocas.jpg",
     slideName: "sixth_slide",
-    text: "lorem ipsum sadasdäasfälaf",
+    text: "lorem ipsum Hallo",
   },
 ];
